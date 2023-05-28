a2f18fcfba57d709ccf2f0c36628e7dd21c4d83d
vitkolos <vit.kolos@gmail.com>
2023-05-28 16:33:55 +0200

    Add analytics tracking to materialy

--- assets/materialy.php
+++ assets/materialy.php
@@ -75,6 +75,26 @@ if (isset($_GET["404"])) {
             color: #aaa;
         }
     </style>
+
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        _paq.push(['disableCookies']);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u = "//www.kolos.ga/matomo/";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d = document,
+                g = d.createElement('script'),
+                s = d.getElementsByTagName('script')[0];
+            g.async = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
 </head>
 
 <body>
