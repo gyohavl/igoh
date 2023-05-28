a875b78d3997e76b9d210075a104512c0bad3897
vitkolos <vit.kolos@gmail.com>
2023-05-28 16:38:36 +0200

    Remove old shortlinks

--- assets/shortener.php
+++ assets/shortener.php
@@ -1,7 +1,6 @@
 <?php
 $shortener = array(
-	'cetba' => 'https://docs.google.com/spreadsheets/d/1drkg0qkQZ3AyMmxbKJq9_tKPwGLyrteiqnTcIj7urkw/edit?usp=sharing',
-	'cetba-kopirovat' => 'https://docs.google.com/spreadsheets/u/1/d/1drkg0qkQZ3AyMmxbKJq9_tKPwGLyrteiqnTcIj7urkw/copy'
+	'example' => 'https://www.igoh.tk/bot/users.php', // https://www.igoh.tk/s/example redirects to https://www.igoh.tk/bot/users.php
 );
 
 if (!empty($_GET['s']) && $shortener[$_GET['s']]) {
