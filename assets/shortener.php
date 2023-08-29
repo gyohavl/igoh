c30d9b6f4b6be2c563c6b0071c917382c5ffac91
vitkolos <vit.kolos@gmail.com>
2023-08-29 14:25:49 +0200

    Trim shortcode slashes

--- assets/shortener.php
+++ assets/shortener.php
@@ -1,11 +1,17 @@
 <?php
 $shortener = array(
 	'example' => 'https://www.igoh.tk/bot/users.php', // https://www.igoh.tk/s/example redirects to https://www.igoh.tk/bot/users.php
-    'burza' => 'https://docs.google.com/spreadsheets/d/17uzXVMzUiakpkmK6Gt65WigDqnwTEjci8lXkLeT-pLg/edit',
+	'burza' => 'https://docs.google.com/spreadsheets/d/17uzXVMzUiakpkmK6Gt65WigDqnwTEjci8lXkLeT-pLg/edit',
 );
 
-if (!empty($_GET['s']) && $shortener[$_GET['s']]) {
-	header('Location: ' . $shortener[$_GET['s']]);
-} else {
-	header('Location: /');
+if (!empty($_GET['s'])) {
+	$s = trim($_GET['s'], '/'); // https://www.igoh.tk/s/example/ is treated like https://www.igoh.tk/s/example
+
+	if (!empty($shortener[$s])) {
+		header('Location: ' . $shortener[$s]);
+		exit;
+	}
 }
+
+header('Location: /');
+exit;
